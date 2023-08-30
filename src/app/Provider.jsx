6f95f6adbdfd5e01ca31d5ea62720752f7b3da90
buraxta <burakgulluler@gmail.com>
2023-08-30 17:59:12 +0300

    create about page

--- src/app/Provider.jsx
+++ src/app/Provider.jsx
@@ -5,7 +5,11 @@ import React from "react";
 const Provider = ({ children }) => {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen select-none">
+      <div
+        className="dark:bg-gray-700 dark:text-gray-200
+       text-gray-700 transition-colors duration-300 
+       min-h-screen select-none"
+      >
         {children}
       </div>
     </ThemeProvider>
---
+++ src/app/about/page.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
+        assumenda. Possimus a error vero dolores consequuntur eligendi
+        perferendis labore provident facilis, dolorum eos molestias numquam ex
+        perspiciatis commodi iste consectetur! Recusandae incidunt deserunt
+        illum cum accusamus soluta harum aspernatur optio, assumenda corrupti
+        itaque ab nam ratione natus iste magni quidem tempore iure delectus
+        voluptatem omnis.
+      </p>
+      <p>
+        Sit id odio incidunt, quidem aperiam accusantium molestias, porro
+        asperiores est, necessitatibus debitis veritatis earum. Sequi architecto
+        debitis adipisci, quia placeat quis dolorem error in ad neque
+        reprehenderit amet tempore suscipit saepe nam commodi consequatur
+        voluptatum nisi minima aut repudiandae.
+      </p>
+      <p>
+        Nihil voluptatem, at provident ipsa earum nemo doloremque id, ipsum
+        delectus amet dolorem quidem possimus? Corporis soluta, ipsam veniam
+        distinctio ratione quasi autem natus, qui neque voluptatibus iure quos,
+        fugit omnis nesciunt doloribus iste dignissimos velit inventore odit
+        praesentium. Consequatur aliquam similique omnis, aut, doloremque ex
+        aliquid in quidem iusto, repellendus commodi minima. Commodi excepturi
+        nemo sint odit eos impedit repellendus earum iure quidem esse? Nesciunt
+        expedita deleniti rem iusto laborum, velit illum repellat dolor minima
+        eaque officia doloremque? Vitae in dolores quia voluptatibus.
+      </p>
+      <p>
+        Ratione voluptas quidem sed ut dicta quod tenetur enim maxime quaerat
+        dolores, quis fuga qui mollitia, totam rem provident autem quos
+        voluptatum quas quisquam nihil non, dignissimos architecto quo? Dolores,
+        ipsam provident hic perferendis exercitationem facilis voluptates
+        obcaecati similique rem quisquam unde vero at distinctio, asperiores,
+        atque saepe? Autem itaque fugiat harum consequuntur voluptates impedit
+        vitae, quidem molestiae, consequatur architecto possimus aspernatur
+        dolorum ullam repellat ad placeat atque cupiditate laudantium incidunt
+        voluptatem, ipsa repudiandae? Et, reprehenderit repellat veritatis,
+        dolore consequuntur impedit at doloribus labore tempore incidunt quia?
+        Temporibus est sed quaerat perspiciatis consectetur? Saepe, rerum illum
+        tempore fugit enim quam voluptas provident eligendi iste perspiciatis
+        expedita sint exercitationem voluptate beatae quaerat eum dolorum
+        consequatur suscipit officiis?
+      </p>
+    </div>
+  );
+};
+
+export default page;
